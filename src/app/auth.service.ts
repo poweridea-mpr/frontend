f1614c0235ed8c34a6ea6209821c14f76a5f035d
Peter Malina <peter.malina@flowup.cz>
2017-04-14 17:11:06 +0200

    fix(auth): looks like I reimplemented the functionality before :/

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -6,15 +6,13 @@ import { AngularFire, FirebaseAuthState } from 'angularfire2';
 @Injectable()
 export class AuthService {
 
-  // subject that represents state of authentication
-  private authState = new BehaviorSubject(null);
-
   // readonly observable from the auth state
   get authState$(): Observable<FirebaseAuthState> {
-    return this.authState.asObservable();
+    return this.af.auth.asObservable();
   }
 
-  constructor(public af: AngularFire) { }
+  constructor(public af: AngularFire) {
+  }
 
   /**
    * logs in an existing user with provided credentials
@@ -22,15 +20,7 @@ export class AuthService {
    * @param password
    */
   login(email: string, password: string): Promise<FirebaseAuthState> {
-    return new Promise((resolve, reject) => {
-      this.af.auth.login({email: `${email}`, password: `${password}`}).then((state: FirebaseAuthState) => {
-        this.authState.next(state);
-        resolve(state);
-      }).catch((e: Error) => {
-        this.authState.next(null);
-        reject(e);
-      });
-    });
+    return <any>this.af.auth.login({email: `${email}`, password: `${password}`})
   }
 
   /**
@@ -39,26 +29,10 @@ export class AuthService {
    * @param password
    */
   register(email: string, password: string): Promise<FirebaseAuthState> {
-    return new Promise((resolve, reject) => {
-      this.af.auth.createUser({email: `${email}`, password: `${password}`}).then((state: FirebaseAuthState) => {
-        this.authState.next(state);
-        resolve(state);
-      }).catch((e: Error) => {
-        this.authState.next(null);
-        reject(e);
-      });
-    });
+    return <any>this.af.auth.createUser({email: `${email}`, password: `${password}`});
   }
 
-  logout(): Promise<FirebaseAuthState> {
-    return new Promise((resolve, reject) => {
-      this.af.auth.logout().then(() => {
-        this.authState.next(null);
-        resolve();
-      }).catch((e: Error) => {
-        this.authState.next(null);
-        reject(e);
-      });
-    });
+  logout(): Promise<void> {
+    return this.af.auth.logout();
   }
 }
