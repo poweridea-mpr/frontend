c13328e9cf979169c73a79e872ba3cabd7466bf7
Peter Malina <peter.malina@flowup.cz>
2017-04-14 09:41:38 +0200

    feat(arch): added landing and platform

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,25 +3,35 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
 
 import { MdCheckboxModule, MdButtonModule,
          MdToolbarModule, MdInputModule, MdListModule,
          MdCardModule, MdCoreModule, MdTooltipModule,
-         MdTabsModule } from '@angular/material';
+         MdTabsModule, MdIconModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { LandingComponent } from './landing/landing.component';
+import { PlatformComponent } from './platform/platform.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    PlatformComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    BrowserAnimationsModule
+    RouterModule,
+    BrowserAnimationsModule,
 
     // material
+    MdCheckboxModule, MdButtonModule,
+    MdToolbarModule, MdInputModule, MdListModule,
+    MdCardModule, MdCoreModule, MdTooltipModule,
+    MdTabsModule, MdIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
