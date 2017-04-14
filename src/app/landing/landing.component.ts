e30abdaa81cc9e474b31588465e703e11c344063
Peter Malina <peter.malina@flowup.cz>
2017-04-14 09:59:22 +0200

    feat(landing): added first login form

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LandingComponent implements OnInit {
 
+  login = {
+    email: '',
+    password: '',
+  };
+
   constructor() { }
 
   ngOnInit() {
