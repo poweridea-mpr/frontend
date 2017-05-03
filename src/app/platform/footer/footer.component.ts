b5f1aab23191351e2e27bee046ff9d54d70a3988
Peter Malina <peter.malina@flowup.cz>
2017-05-03 23:46:16 +0200

    feat(footer): integrated stats

--- src/app/platform/footer/footer.component.ts
+++ src/app/platform/footer/footer.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2';
+import { Project, Risk } from '../../models';
 
 @Component({
   selector: 'app-footer',
@@ -7,7 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+  projects: FirebaseListObservable<Project[]>;
+  risks: FirebaseListObservable<Risk[]>;
+
+  totalProjects: number = 0;
+  totalRisks: number = 0;
+  totalValue: number = 0;
+
+  constructor(public db: AngularFireDatabase) {
+    this.projects = db.list('/projects');
+    this.risks = db.list('/risks');
+
+    this.projects.subscribe((projs: Project[]) => {
+      this.totalProjects = projs.length;
+    });
+
+    this.risks.subscribe((risks: Risk[]) => {
+      this.totalRisks = risks.length;
+      this.totalValue = risks.reduce((acc, risk) => +acc + (risk.value || 0), 0);
+    })
+  }
 
   ngOnInit() {
   }
