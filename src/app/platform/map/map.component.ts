baad8a6b485af7e478d530d0868088b7fcc19a3b
Albert Uchytil <albert.uchytil@flowup.cz>
2017-05-03 21:42:16 +0200

    feat(map): added basic gantt chart

--- src/app/platform/map/map.component.ts
+++ src/app/platform/map/map.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFire } from 'angularfire2';
+import { Risk } from '../../models/risk.model';
 
 @Component({
   selector: 'app-map',
@@ -8,9 +9,42 @@ import { AngularFire } from 'angularfire2';
 })
 export class MapComponent implements OnInit {
 
-  constructor() { }
+  riskData: Risk[] = [];
 
-  ngOnInit() {
+  constructor(public af: AngularFire) {
+    this.af.database.list('/risks').subscribe(data => {
+      this.riskData = data;
+      this.drawChart();
+    });
   }
 
+  ngOnInit() {
+    google.charts.load('current', {packages: ['gantt']});
+    //google.charts.setOnLoadCallback(this.drawChart);
+  }
+
+  drawChart() {
+    var data = new google.visualization.DataTable();
+    data.addColumn('string', 'Task ID');
+    data.addColumn('string', 'Task Name');
+    data.addColumn('date', 'Start Date');
+    data.addColumn('date', 'End Date');
+    data.addColumn('number', 'Duration');
+    data.addColumn('number', 'Percent Complete');
+    data.addColumn('string', 'Dependencies');
+
+    this.riskData.map(item => {
+      data.addRows([[item.project, item.name, item.from, item.to, null,  100,  null]]);
+    });
+
+    var options = {
+      height: 400,
+      gantt: {
+        trackHeight: 30
+      }
+    };
+
+    var chart = new google.visualization.Gantt(document.getElementById('chart'));
+    chart.draw(data, options);
+  }
 }
